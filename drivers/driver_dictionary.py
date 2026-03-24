8cc63db98e894a42cdc5c240f967b0ef900c169b
Christine O <83026145+drchristinee1@users.noreply.github.com>
2026-03-24 09:25:31 -0400

    Update driver_dictionary.py

--- drivers/driver_dictionary.py
+++ drivers/driver_dictionary.py
@@ -1,1 +1,29 @@
-
+STANDARD_DRIVERS = {
+    "Compute_Time": [
+        "ec2_hours",
+        "lambda_duration_gb_seconds",
+        "fargate_hours"
+    ],
+    "Request_Count": [
+        "lambda_invocations",
+        "api_requests"
+    ],
+    "Capacity_Units": [
+        "dynamodb_rcu",
+        "dynamodb_wcu"
+    ],
+    "Storage_Volume": [
+        "s3_gb",
+        "ebs_gb",
+        "rds_storage_gb"
+    ],
+    "Throughput": [
+        "data_transfer_gb",
+        "nat_gateway_gb"
+    ],
+    "IO_Performance": [
+        "iops",
+        "read_ops",
+        "write_ops"
+    ]
+}
